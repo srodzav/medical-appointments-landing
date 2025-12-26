53ddc55406db0461d771757f410e87aebf747bbd
Sebastian Zavala <mrjchzz@gmail.com>
2025-12-26 13:15:48 -0600

    fix: change treatments contact button and action

--- frontend/src/app/features/treatments/treatments.component.ts
+++ frontend/src/app/features/treatments/treatments.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,6 +10,8 @@ import { RouterModule } from '@angular/router';
   styleUrl: './treatments.component.scss',
 })
 export class TreatmentsComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  mobileOpen = false;
   treatments = [
     {
       icon: 'straighten',
@@ -40,4 +42,30 @@ export class TreatmentsComponent {
       popular: false,
     },
   ];
+
+  closeAll() {
+    this.mobileOpen = false;
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.style.overflow = '';
+    }
+  }
+
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    this.closeAll();
+
+    if (isPlatformBrowser(this.platformId)) {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        const headerOffset = 0;
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth',
+        });
+      }
+    }
+  }
 }
